20191c7798b6454044f0a2935632665c2e5982bc
PikachuDeveloper <shevacharmeleon@gmail.com>
2021-12-30 22:35:51 +0200

    intermediate result

--- OrdersParser.py
+++ OrdersParser.py
@@ -1,44 +1,73 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 from bs4 import BeautifulSoup
+import re
+# pip install beautifulsoup4
+product_name = "ali-kit_Base__base__1odrub ali-kit_Base__default__1odrub ali-kit_Link__link__cmgtoz ali-kit_Link__size-xs__cmgtoz ali-kit_Link__default__cmgtoz OrderList_TradeProductInfo__productTitle__18tx4"
+
+def tag2json(row, pattern=True):
+    draft = r'Order ID:.(?P<orderNum>\d+).View DetailOrder time:.Store name:.(?P<storeName>.+)View StoreContact SellerOrder amount:.\$ (?P<fullprice>\d+\.\d\d)(?P<product>.+)\[Transaction Screenshot\]\$ (?P<price_per_1>\d+\.\d\d) X(?P<amount>\d+)'
+    price = r'\$ (?P<price_per_1>\d+\.\d\d) X(?P<amount>\d+)'
+    products = soup.find_all(class_=product_name)
+    if row.count('[Transaction Screenshot]') == 1 and pattern:
+        info = re.finditer(draft, row)
+        for m in info:
+            return {'shop': m.group('storeName'), 'price_per_1': m.group('price_per_1'),
+                   'order ID': m.group('orderNum'), 'paid': m.group('fullprice'),
+                   'product': m.group('product'), 'amount': m.group('amount')}
+    elif row.count('[Transaction Screenshot]') == 1:
+        for tag in products:
+            if tag.get_text() in row:
+                n = re.finditer(price, row)
+                return {'shop': m.group('storeName'), 'price_per_1': n.group('price_per_1'),
+                         'order ID': m.group('orderNum'), 'paid': m.group('fullprice'),
+                         'product': tag.get_text(), 'amount': n.group('amount')}
+    else:
+        x = {}
+        parts = row.split('[Transaction Screenshot]')[:2]
+        part = '[Transaction Screenshot]'.join(row.split('[Transaction Screenshot]')[:2])
+        x.update(tag2json(part, True))
+        for i in range(1, len(parts)-1):
+            part = '[Transaction Screenshot]'.join(row.split('[Transaction Screenshot]')[:2])
+            x.update(tag2json(part, False))
+        return x
+
 
 with open('AliExpressTransactions.htm', 'r', encoding="utf-8") as f:
 
     #contents = f.read()
-
+    x = []
     soup = BeautifulSoup(f, 'html.parser')
     mydivs = soup.findAll("div", {"class": None})
-    class_ = "ali-kit_Base__base__1odrub ali-kit_Base__default__1odrub ali-kit_Label__label__1n9sab ali-kit_Label__size-xs__1n9sab"
+    class_num = "ali-kit_Base__base__1odrub ali-kit_Base__default__1odrub ali-kit_Label__label__1n9sab ali-kit_Label__size-xs__1n9sab"
+    product_name = "ali-kit_Base__base__1odrub ali-kit_Base__default__1odrub ali-kit_Link__link__cmgtoz ali-kit_Link__size-xs__cmgtoz ali-kit_Link__default__cmgtoz OrderList_TradeProductInfo__productTitle__18tx4"
+    orders = soup.findAll("span", {"class": class_num})
     # id = "ali-kit_Base__base__1odrub ali-kit_Base__default__1odrub price ali-kit_Price__size-xs__12ybyf OrderList_TradeProductInfo__productAmount__18tx4"
     # seven_day = soup.find(id="seven-day-forecast")
-    d = soup.div
-    forecast_items = soup.find_all(class_=class_)
-    forecast_items = list(map(lambda x: x.get_text(), forecast_items))
-    forecast_items = list(filter(('').__ne__, forecast_items))
+    shopNum = soup.find_all(class_=class_num)
+    shopNum = list(map(lambda x: x.get_text(), shopNum))
+    shopNum = list(filter(('').__ne__, shopNum))
+    draft = r'Order ID:.(?P<orderNum>\d+).View DetailOrder time:.Store name:.(?P<storeName>.+)View StoreContact SellerOrder amount:.\$ (?P<fullprice>\d+\.\d\d)(?P<product>.+)\[Transaction Screenshot\]\$ (?P<price_per_1>\d+\.\d\d) X(?P<amount>\d+)'
+    price = r'\$ (?P<price_per_1>\d+\.\d\d) X(?P<amount>\d+)'
+    info = re.finditer(draft, mydivs[0].get_text())
+    for m in info:
+        print(m.group('orderNum'))
+    # class_prod = "ali-kit_Base__base__1odrub ali-kit_Base__default__1odrub ali-kit_Link__link__cmgtoz ali-kit_Link__size-xs__cmgtoz ali-kit_Link__default__cmgtoz OrderList_TradeProductInfo__productTitle__18tx4"
+    # products = soup.findAll("a", {"class": class_prod})
+    #
+    # for i in products:
+    #     print(i.get_text())
 
     #prints everything
-    for i in mydivs:
-        if 'Order ID:' == i.get_text()[:9]:
-            print(i.get_text())
+    for obj in mydivs.copy():
+        if 'Order ID:' != obj.get_text()[:9] or "$" not in obj.get_text():
+            mydivs.remove(obj)
+    for box in mydivs:
+        if box.get_text().count('[Transaction Screenshot]'):
+            x.append(tag2json(box.get_text()))
 
-    #prints only order number and shop name
-    # for i in range(0, len(forecast_items), 4):
-    #     print('Order number: ', forecast_items[i])
-    #     print('Shop: ', forecast_items[i+1])
-    #     print('?: ', forecast_items[i+2])
-    #     print('?: ', forecast_items[i+3])
-    #     print('---------------------------------------------')
+    for el in x:
+        print(el)
+    # print(x)
+    print(len(x))
 
-
-#soup = BeautifulSoup(contents, 'html')
-# class="OrderList_OrderItem__row__9lg56 OrderList_OrderItem__productItemWrap__9lg56"
-#
-# class ="OrderList_OrderItem__row__9lg56 OrderList_OrderItem__productItemWrap__9lg56"
-# class ="OrderList_OrderItem__row__9lg56"
-# class ="ali-kit_Col__col__1lcv7b ali-kit_Col__col-6__1lcv7b"
-# class ="OrderList_TradeProductInfo__wrapper__18tx4 OrderList_OrderItem__productCell__9lg56"
-#
-# class ="ali-kit_Base__base__1odrub ali-kit_Base__light__1odrub ali-kit_Label__label__1n9sab ali-kit_Label__size-xs__1n9sab"
-#
-#
-# class ="product-policy_ProductPolicy__wrapper__to6hoe"
